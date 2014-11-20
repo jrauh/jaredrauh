e1740788a363f03d6123f526f9d8f2a89f35b058
Jared Rauh <jaredrauh@gmail.com>
2014-11-19 20:51:21 -0600

    Revert "Initial push"
    This reverts commit f39661ba3b81204b5fc5051ebacd7899e46701d1.

--- rauh.js
+++
@@ -1,6 +0,0 @@
-function overlay() {
-	el = document.getElementById("overlay");
-	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
-}
-
-document.addEventListener("touchstart", function(){}, true);
